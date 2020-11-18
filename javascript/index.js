b18413901df5606622309ddbdeee8a1a387fb2a2
magacarolina <mparra@udesa.edu.ar>
2020-11-18 14:17:06 -0300

    commit ultimo

--- javascript/index.js
+++
@@ -1,6 +0,0 @@
-console.log("verifica");
-
-var peliculas=[
-    /*lista de objetos q sean peliculas q tenga nombre e img y un ID
-    dsp borrar del index.html los article y generarlos a traves de jscript con un foreach*/
-]
