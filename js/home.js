413d99e95b693a91ec29ca60515d5861b4d548fb
magacarolina <mparra@udesa.edu.ar>
2020-11-19 10:31:29 -0300

    home

--- js/home.js
+++ js/home.js
@@ -33,9 +33,9 @@ fetch (url)
         console.log(error);
     })
 
-media = "tv";
-timeWindow = "week";          
-fetch (url)
+   
+let url2 = `https://api.themoviedb.org/3/trending/tv/week?api_key=${apiKey}`       
+fetch (url2)
 .then(function (respuesta){
  return respuesta.json()
  })
@@ -44,9 +44,38 @@ fetch (url)
 let info = data.results 
 let tv = document.querySelector('.top-series');
                         
-for (let i=0; i<6; i++){
+for (let i=0; i<10; i++){
 tv.innerHTML += 
                                                     `<article class="series">
+                                                    <a href="detallepelicula.html?id=${info[i].id}">
+                                                        <div class="zoom">
+                                                            <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" alt="${info[i].title}" >
+                                                             <h3>${info[i].name}</h3>
+                                                        </div>
+                                                     </a>
+                                                     </article>`
+                        
+                                                    }
+                        
+                                                })
+
+.catch (function (error){
+console.log(error);
+ })
+
+let url3 = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}language=en-US&with_genres=10751`;       
+fetch (url3)
+.then(function (respuesta){
+ return respuesta.json()
+ })
+.then (function (data){
+                        
+let info = data.results 
+let kids = document.querySelector('.top-kids');
+                        
+for (let i=0; i<10; i++){
+kids.innerHTML += 
+                                                    `<article class="pelicula">
                                                     <a href="detallepelicula.html?id=${info[i].id}">
                                                         <div class="zoom">
                                                             <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" alt="${info[i].title}" >
@@ -62,3 +91,5 @@ tv.innerHTML +=
 .catch (function (error){
 console.log(error);
  })
+
+ 
