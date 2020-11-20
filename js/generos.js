97a97eedb42e259898f46378f699a3cf576b99f6
magacarolina <mparra@udesa.edu.ar>
2020-11-20 12:05:15 -0300

    generos

--- js/generos.js
+++ js/generos.js
@@ -1,4 +1,4 @@
-let url = `https://api.themoviedb.org/3/genre/movie/list?api_key=7b1d579cd6ba8b41cc1f3f375e375cb5&language=en-US`
+let url = `https://api.themoviedb.org/3/genre/movie/list?api_key=7b1d579cd6ba8b41cc1f3f375e375cb5`
 
 fetch (url)
     .then(function (respuesta){
@@ -6,26 +6,22 @@ fetch (url)
     })
     .then (function (genres){
 
-        let info = genres 
-        let generos = document.querySelector('.generos');
+        let genre = genres.genres
+        let generos = document.querySelector('.genre');
+       console.log(genres);
 
-        for (let i=0; i<length; i++){
-            generos.innerHTML += 
-                `  <section class="generos">
+        for (let i=0; i<genre.length; i++){ 
+          generos.innerHTML += 
 
-                <h2>Géneros</h2>
-                
-                  <ul>
-                        <li>
-                          <a href="">
-                            <h1>${info[i].name} </h1> 
-                          <img src="img/generos img/accion.jpg" alt="accion">
-                        </a>
-                      </li>
-                      </section>`
-
+            `<li>
+                <a href="">
+                     <h1 class="genero">${genre[i].name} </h1> 
+                     <img src="img/generos_img/${genre[i].name}.jpg" alt="${genre[i].name}">
+                    </a>
+                  </li>`
     }
 })
 .catch (function (error){
     console.log(error);
 })
+
