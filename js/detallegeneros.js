6111ed1d345975cfe6a1db0a7583b730f275d736
martinaunamunzaga <munamunzaga@udesa.edu.ar>
2020-11-26 16:22:23 -0300

    commit mar

--- js/detallegeneros.js
+++ js/detallegeneros.js
@@ -3,38 +3,38 @@ let queryString = window.location.search;
 
 let queryObject = new URLSearchParams(queryString);
 
-let genres = queryObject.get("id");
+let id = queryObject.get("id");
 
 
 let apiKey = "7b1d579cd6ba8b41cc1f3f375e375cb5"
 
 
-let url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&with_genres=${genres}`
+let url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&with_genres=${id}`
 
 fetch (url)
-    .then(function (respuesta){
-        return respuesta.json()
+    .then(function (response){
+        return response.json()
     })
     .then (function (data){
-        console.log(data);
-        let infoGeneros = data.results 
-        let generos = document.querySelector('.generos');
-        for (let i=0; i<length; i++){
-            generos.innerHTML += 
-                            `<h2></h2>
-                            <article class="generos">
-                            <a href="detallepelicula.html?id=${infoGeneros[i].id}">
-                                <div class="zoom">
-                                    <img src="https://image.tmdb.org/t/p/w500${infoGeneros[i].poster_path}" alt="${info[i].title}" >
-                                     <h3>${infoGeneros[i].title}</h3>
-                                </div>
-                             </a>
-                             </article>`
-
-                            }
-
-                        })
         
+       let infoGeneros = data.results 
+       console.log(infoGeneros)
+        let generos = document.querySelector('.generos');
+        
+             for (let i=0; i<8; i++){
+                         let pelicula = `<h2></h2>
+                         <article class="peliculas">
+                         <a href="detallepelicula.html?id=${infoGeneros[i].id}">
+                             <div class="zoom">
+                                 <img src="https://image.tmdb.org/t/p/w500${infoGeneros[i].poster_path}" alt="${infoGeneros[i].title}" >
+                                  <h3>${infoGeneros[i].title}</h3>
+                             </div>
+                          </a>
+                          </article>`;
+                          generos.innerHTML += pelicula 
+             }
+          
+    })  
     .catch (function (error){
         console.log(error);
     })
